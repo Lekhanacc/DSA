5805c192bf93d629930ca2d18ddc13d8425b4856
Lekhana.C <clekhana224@gmail.com>
2026-01-23 21:19:46 +0530

    Time: 28 ms (50.02%) | Memory: 75 MB (95.42%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+class Solution{
+    public int[] sortArray(int[] nums){
+        if(nums.length<=1){
+            return nums;
+        }
+        int mid = nums.length/2;
+        int[] left=sortArray(Arrays.copyOfRange(nums,0,mid));
+        int[] right=sortArray(Arrays.copyOfRange(nums,mid,nums.length));
+        return merge(left,right);
+    }
+    private int[] merge(int[] first,int[] second){
+        int[] mix= new int[first.length+second.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<first.length&&j<second.length){
+            if(first[i]<second[j]){
+                mix[k]=first[i];
+                i++;
+            }else{
+                mix[k]=second[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<first.length){
+            mix[k]=first[i];
+            i++;
+            k++;
+        }
+        while(j<second.length){
+            mix[k]=second[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+} 
+
+
+// import java.util.Arrays;
+// class Solution {
+//     public void main(String[] args){
+//         int[] arr = {5,1,1,2,0,0};
+//         sortArray(arr);
+//         System.out.println(Arrays.toString(arr));
+//     }
+//     public int[] sortArray(int[] arr) {
+//         int n = arr.length;
+//         for(int i =0;i<n;i++) {
+//             int min = i;
+//             for (int j = i + 1; j < n; j++) {
+//                 if (arr[j] < arr[min]) {
+//                     min = j;
+//                 }
+//             }
+//             int temp = arr[min];
+//             arr[min] = arr[i];
+//             arr[i] = temp;
+//         }
+//         return arr;
+//     }
+// }

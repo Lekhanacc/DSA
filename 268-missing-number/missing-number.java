9e2d45429e7ae54fac369c0721fdeafd77205074
Lekhana.C <clekhana224@gmail.com>
2026-01-24 18:21:17 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (26.48%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,31 +1,31 @@
-import java.util.Arrays;
-
-class Solution {
-    public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i) {
-                return i;
-            }
-        }
-
-        // If all indices matched
-        return nums.length;
-    }
-}
-
+// import java.util.Arrays;
 
 // class Solution {
-//     public int missingNumber(int[] arr) {
-//         int n = arr.length;
-//         int expectedSum = n * (n + 1) / 2; // Sum of first n natural numbers
-//         int actualSum = 0;
+//     public int missingNumber(int[] nums) {
+//         Arrays.sort(nums);
 
-//         for (int num : arr) {
-//             actualSum += num;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] != i) {
+//                 return i;
+//             }
 //         }
 
-//         return expectedSum - actualSum;
+//         // If all indices matched
+//         return nums.length;
 //     }
 // }
+
+
+class Solution {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        int expectedSum = n * (n + 1) / 2; // Sum of first n natural numbers
+        int actualSum = 0;
+
+        for (int num : arr) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+}

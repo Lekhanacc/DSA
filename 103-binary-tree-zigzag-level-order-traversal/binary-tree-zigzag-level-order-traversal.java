9065f81be455e1a7d27e3288e655a02f470c2edd
Lekhana.C <clekhana224@gmail.com>
2026-07-09 21:24:31 +0530

    Time: 1 ms (80.36%) | Memory: 43.8 MB (25.11%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,7 +14,54 @@
  * }
  */
 
- class Solution {
+// ******************* O (N**2)  *************************
+
+//  class Solution {
+//     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+//         List<List<Integer>> ans = new ArrayList<>();
+
+//         if (root == null)
+//             return ans;
+
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.offer(root);
+
+//         boolean leftToRight = true;
+
+//         while (!queue.isEmpty()) {
+
+//             int size = queue.size();
+//             List<Integer> level = new ArrayList<>();
+
+//             for (int i = 0; i < size; i++) {
+
+//                 TreeNode current = queue.poll();
+
+//                 if (leftToRight)
+//                     level.add(current.val);
+//                 else
+//                     level.add(0, current.val);
+
+//                 if (current.left != null)
+//                     queue.offer(current.left);
+
+//                 if (current.right != null)
+//                     queue.offer(current.right);
+//             }
+
+//             ans.add(level);
+
+//             leftToRight = !leftToRight;
+//         }
+
+//         return ans;
+//     }
+// }
+
+// *************   O(N)   *******************
+
+class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 
         List<List<Integer>> ans = new ArrayList<>();
@@ -30,16 +77,26 @@
         while (!queue.isEmpty()) {
 
             int size = queue.size();
+
             List<Integer> level = new ArrayList<>();
+
+            // Fill the list with dummy values
+            for (int i = 0; i < size; i++) {
+                level.add(0);
+            }
 
             for (int i = 0; i < size; i++) {
 
                 TreeNode current = queue.poll();
 
+                int index;
+
                 if (leftToRight)
-                    level.add(current.val);
+                    index = i;
                 else
-                    level.add(0, current.val);
+                    index = size - 1 - i;
+
+                level.set(index, current.val);
 
                 if (current.left != null)
                     queue.offer(current.left);
